37c143e05bb6286b57b2726b507fa1415ae3f853
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-06-16 22:44:36 -0400

    add UI

--- db/src/main/java/org/example/platzi/main/Main.java
+++ db/src/main/java/org/example/platzi/main/Main.java
@@ -1,31 +1,17 @@
 package org.example.platzi.main;
 
-import org.example.platzi.model.Employee;
-import org.example.platzi.repository.EmployeeRepository;
-import org.example.platzi.repository.Repository;
-import org.example.platzi.util.DatabaseConnection;
+import org.example.platzi.view.SwingApp;
+
+//import org.example.platzi.model.Employee;
+//import org.example.platzi.repository.EmployeeRepository;
+//import org.example.platzi.repository.Repository;
+//import org.example.platzi.util.DatabaseConnection;
 
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        try (Connection myConnection = DatabaseConnection.getInstance()) {
-            Repository<Employee> repository = new EmployeeRepository();
-
-            System.out.println("List all");
-            repository.findAll().forEach(System.out::println);
-
-            System.out.println("------Insert Employee------");
-            Employee employee = new Employee();
-            employee.setFirst_name("Carlos");
-            employee.setPa_surname("Palacios");
-            employee.setMa_surname("Ramos");
-            employee.setEmail("carlosargelio0104@mail.com");
-            employee.setSalary((float)100.54);
-            repository.save(employee);
-
-            System.out.println("List all two");
-            repository.findAll().forEach(System.out::println);
-        }
+        SwingApp app = new SwingApp();
+        app.setVisible(true);
     }
 }
